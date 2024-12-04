1354929b0bf5a9c03ffcf8706c24bb05f1b3698f
Alessandro De Blasis <alex@deblasis.net>
2024-12-04 05:47:31 +0100

    more stuff
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- cmd/pull-watch/main.go
+++ cmd/pull-watch/main.go
@@ -8,7 +8,14 @@ import (
 	"github.com/ship-digital/pull-watch/internal/runner"
 )
 
+var version = "dev"
+
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Printf("pull-watch version %s\n", version)
+		return
+	}
+
 	cfg, err := config.Parse()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
